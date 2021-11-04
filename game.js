35b277304a334ec2611a4a4012290c44dd71554a
Daniel Marino <starzonmyarmz@gmail.com>
2021-11-04 10:05:42 -0400

    Tweaks and collision

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-let WIDTH = 360
-let HEIGHT = 620
+let WIDTH = 320
+let HEIGHT = 480
 
 let bug
 let title, home, twitter
@@ -26,27 +26,34 @@ function draw() {
   }
 
   if (title.complete && !home.complete) {
+    hits = []
     home.setTimeStart(millis())
     home.draw()
     home.update(millis())
 
-    // home.shapes.forEach((s, i) => {
-    //   hits[i] = collideRectCircle(s.pos.x, s.pos.y, s.w, s.h, bug.pos.x, bug.pos.y, bug.radius)
-    // })
+    home.shapes.forEach((s, i) => {
+      hits[i] = collideRectCircle(s.pos.x, s.pos.y, s.w, s.h, bug.pos.x, bug.pos.y, bug.radius)
+    })
   }
 
   if (home.complete && !twitter.complete) {
+    hits = []
     twitter.setTimeStart(millis())
     twitter.draw()
     twitter.update(millis())
 
-    // twitter.shapes.forEach((s) => {
-    //   hit = collideRectCircle(s.pos.x, s.pos.y, s.w, s.h, bug.pos.x, bug.pos.y, bug.radius)
-    // })
+    twitter.shapes.forEach((s, i) => {
+      hits[i] = collideRectCircle(s.pos.x, s.pos.y, s.w, s.h, bug.pos.x, bug.pos.y, bug.radius)
+    })
   }
 
   bug.draw()
   bug.update(mouseX, mouseY)
+
+  hits.forEach((h) => {
+    if (h) console.log(h)
+  })
+
 }
 
 function doubleClicked() {
