e86388a3b3c684a5ec0f9ce88bdeced51397974c
Daniel Marino <starzonmyarmz@gmail.com>
2021-11-04 19:53:46 -0400

    dynamic alpha and more fleshed out home

--- rect.js
+++ rect.js
@@ -1,16 +1,15 @@
 class Rect {
-  constructor(x, y, w, h, radius = 0, alpha = 1) {
+  constructor(x, y, w, h, alpha = 1) {
     this.pos = createVector(x, y)
     this.vel = createVector(0, 0)
     this.w = w
     this.h = h
-    this.radius = radius
     this.alpha = alpha
   }
 
   draw() {
-    fill(255)
-    rect(this.pos.x, this.pos.y, this.w, this.h, this.radius)
+    fill(`rgba(255, 255, 255, ${this.alpha})`)
+    rect(this.pos.x, this.pos.y, this.w, this.h)
   }
 
   updateCoordinates(x, y) {
--- title.js
+++ title.js
@@ -3,9 +3,6 @@ class Title {
   }
 
   draw() {
-    fill(255)
-    noStroke()
-
-    new Rect(width - 10, height - 10, 10, 10, 0, 0, 0).draw()
+    new Rect(width - 10, height - 10, 10, 10, 1).draw()
   }
 }
