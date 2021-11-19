2a12230295f61118fd32de07626251e054ed92e2
starzonmyarmz <starzonmyarmz@users.noreply.github.com>
2021-11-19 21:25:39 +0000

    deploy: 884768c733da9e210b5d8cfbbc902d2b2ccfc686

---
+++ src/scene_youwin.js
@@ -0,0 +1,1 @@
+class YouWin extends Scene{constructor(){super(),this.reset()}draw(){background(255),text("you win placeholder lol",0,0)}reset(){this.song_started=!1,this.song_stopped=!1,this.shapes=[]}}
