38fd5b8f2afa227afa9ebe8d71777fa8502417eb
Daniel Marino <starzonmyarmz@gmail.com>
2021-11-26 14:10:23 -0500

    Adjust twitter layout to prevent cheesing

--- src/scene_twitter.js
+++ src/scene_twitter.js
@@ -43,23 +43,23 @@ class Twitter extends Scene {
 
     const color = '#1d9bf0'
 
-    let next_y = 16
+    let next_y = 24
     let dur = 60000
     let dist = 4000
 
     // Tweet
     let createTweet = (y) => {
       this.shapes.push(
-        new Poly({ x: 16, y: y - dist, d: 48, shape: 'circle', color: color }, [
+        new Poly({ x: -4, y: y - dist, d: 48, shape: 'circle', color: color }, [
           { delay: 0, duration: dur, props: { y: y } }
         ]),
-        new Poly({ x: 78, y: y + 12 - dist, w: random(48, 144), h: 8, color: color }, [
+        new Poly({ x: 56, y: y + 12 - dist, w: random(48, 144), h: 8, color: color }, [
           { delay: 0, duration: dur, props: { y: y + 12 } }
         ]),
-        new Poly({ x: 78, y: y + 28 - dist, w: random(48, 144), h: 8, color: color }, [
+        new Poly({ x: 56, y: y + 28 - dist, w: random(48, 144), h: 8, color: color }, [
           { delay: 0, duration: dur, props: { y: y + 28 } }
         ]),
-        new Poly({ x: 296, y: y + 14 - dist, w: 16, h: 4, color: color }, [
+        new Poly({ x: 300, y: y + 14 - dist, w: 16, h: 4, color: color }, [
           { delay: 0, duration: dur, props: { y: y + 14 } }
         ])
       )
@@ -68,13 +68,13 @@ class Twitter extends Scene {
 
       for (let i = 0; i <= l; i++) {
         this.shapes.push(
-          new Poly({ x: 16, y: y + 60 + 12 * i - dist, w: (i === l ? random(48, 176) : random(248, 288)), h: 4, color: color }, [
+          new Poly({ x: 56, y: y + 60 + 12 * i - dist, w: (i === l ? random(48, 176) : random(248, 240)), h: 4, color: color }, [
             { delay: 0, duration: dur, props: { y: y + 60 + 12 * i} }
           ])
         )
       }
 
-      next_y = next_y + 88 + (l * 12)
+      next_y = next_y + 96 + (l * 12)
 
       let p = random() > 0.5
 
@@ -82,7 +82,7 @@ class Twitter extends Scene {
         let h = random(125, 250)
 
         this.shapes.push(
-          new Poly({ x: 16, y: next_y - dist, w: 288, h: h, color: color }, [
+          new Poly({ x: 56, y: next_y - dist, w: 240, h: h, color: color }, [
             { delay: 0, duration: dur, props: { y: next_y } }
           ])
         )
@@ -91,7 +91,7 @@ class Twitter extends Scene {
       }
     }
 
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < 35; i++) {
       createTweet(next_y)
     }
 
